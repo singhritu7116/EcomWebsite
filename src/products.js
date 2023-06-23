555cbbc44aba2a70c2c90ac1f75eec567febdc06
RITU KUMARI <102386249+singhritu7116@users.noreply.github.com>
2023-06-23 23:53:11 +0530

    Initial commit

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/1.png";
+import product2 from "./assets/products/2.png";
+import product3 from "./assets/products/3.png";
+import product4 from "./assets/products/4.png";
+import product5 from "./assets/products/5.png";
+import product6 from "./assets/products/6.png";
+import product7 from "./assets/products/7.png";
+import product8 from "./assets/products/8.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Zara Cargo Fit Pant for Men",
+    price: 1999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Waterproof,longlasting Watch",
+    price: 699.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Backrest",
+    price: 399.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Prestige Mixer,Grinder Set",
+    price: 1200.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Sony Headphones",
+    price: 2599.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SmartWatch",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Earings",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Gucci Dionysus Bamboo LeatherBag",
+    price: 40.0,
+    productImage: product8,
+  },
+];
